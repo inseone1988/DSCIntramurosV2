89db5b14a964c7b3172911b3d1093227ac6ad3b4
Javier Ramirez <jimlombriz1988@gmail.com>
2019-02-21 11:28:47 -0600

    Added DrawerLayout

---
+++ app/src/main/java/mx/com/vialogika/dscintramurosv2/Room/AppDatabase.java
@@ -0,0 +1,8 @@
+package mx.com.vialogika.dscintramurosv2.Room;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {},version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+}
